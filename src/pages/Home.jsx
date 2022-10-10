f366a589b95fd4859a42f33bda3535a9ce5d5777
ke975 <60978634+ke975@users.noreply.github.com>
2022-10-10 12:24:02 -0600

    barra event

--- src/pages/Home.jsx
+++ src/pages/Home.jsx
@@ -35,7 +35,7 @@ async function handleSubscribe(e){
         name, email
       }
     })
-    navigate('event')
+    navigate('/event')
   } catch (error) {
     alert("Los datos estan vacios por favor llene los campos")
   }
