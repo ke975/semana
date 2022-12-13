62d71d302c1e368249521671b7b826a9a21c2b73
ke975 <60978634+ke975@users.noreply.github.com>
2022-12-12 20:54:03 -0600

    fsfsdfs

--- src/routes.jsx
+++ src/routes.jsx
@@ -1,5 +1,5 @@
 import {Route,Routes} from "react-router-dom";
-import {Home} from './pages/Home'
+
 import {Event} from './pages/Event'
 import {Prueba} from './pages/prueba'
 
@@ -8,8 +8,8 @@ import {Prueba} from './pages/prueba'
 export function Router(){
     return(
 <Routes>
-  <Route path="/" element={<Home/>}/>
-  <Route path="/event" element={<Event/>}/>
+
+  <Route path="/" element={<Event/>}/>
   <Route path="/event/lesson/:slug" element={<Event/>}/>
   <Route path="/event/prueba" element={<Prueba/>}/>
 </Routes>
